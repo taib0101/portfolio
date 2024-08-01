14ef17d20c1936c983f6fd44978371dd7b0253db
taib0101 <taibvai13@gmail.com>
2024-08-01 16:27:59 +0600

    updated aug1_16.27

---
+++ toggler.js
@@ -0,0 +1,23 @@
+const hamburgerDisplayOpen = document.querySelector("#hamburgerDisplayOpen");
+const hamburgerDisplayParent = document.querySelector("#hamburgerDisplayParent");
+const hamburgerDisplayChild = document.querySelector("#hamburgerDisplayChild");
+const hamburgerDisplayClose = document.querySelector("#hamburgerDisplayClose");
+
+hamburgerDisplayOpen.addEventListener("click" , (event) => {
+    event.preventDefault();
+    // console.log("hamburgerDisplayOpen clicked");
+    hamburgerDisplayParent.style.right = "0";
+});
+
+hamburgerDisplayParent.addEventListener("click" , (event) => {
+    hamburgerDisplayParent.style.right = "-10000px";
+});
+
+
+hamburgerDisplayChild.addEventListener("click" , (event) => {
+    event.stopPropagation();
+});
+
+hamburgerDisplayClose.addEventListener("click" , (event) => {
+    hamburgerDisplayParent.style.right = "-10000px";
+});
